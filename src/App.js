b46dd84dcc390a2855ce802556a912e795d7fe80
Zidan Mehedi <55471629+zidanMehedi@users.noreply.github.com>
2020-12-08 21:09:16 +0600

    Loading time reduced(4)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar'
 import Home from './Home';
 import About from './About';
@@ -7,9 +7,9 @@ import Skills from './Skills';
 import Contact from './Contact';
 import Footer from './Footer';
 import Reset from './Reset';
+import Preloader from './Preloader'
 
-function App() {
-
+function MyApp() {
   return (
     <>
       <Reset />
@@ -20,6 +20,26 @@ function App() {
       <Skills />
       <Contact />
       <Footer />
+    </>
+  );
+}
+
+function App() {
+
+  const [customRender, setCustomRender] = useState(<Preloader/>);
+
+  var setPromise = () => { 
+    return new Promise((promise) => {setTimeout(promise(), 5000)}); 
+  }
+  useEffect(()=>{
+    
+    setPromise().then(setCustomRender(<MyApp/>))
+  },[])
+
+
+  return (
+    <>
+      {customRender}
     </>
   );
 
