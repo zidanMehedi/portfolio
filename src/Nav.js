712e4a4c55382b72d5e43216bafd22ddc7a209c3
zidanMehedi <mkzzidan786@gmail.com>
2021-01-26 02:58:32 +0600

    route fixed

--- src/Nav.js
+++ src/Nav.js
@@ -2,32 +2,32 @@ const nav = [
     {
         key: 1,
         name: 'Home',
-        //path: 'localhost:3000/portfolio',
-        path : 'https://zidanMehedi.github.io/portfolio'
+        //path: 'localhost:3000/portfolio/#',
+        path : 'https://zidanMehedi.github.io/portfolio/#'
     },
     {
         key: 2,
         name: 'About',
-        //path: 'localhost:3000/portfolio',
-        path : 'https://zidanMehedi.github.io/portfolio'
+        //path: 'localhost:3000/portfolio/#',
+        path : 'https://zidanMehedi.github.io/portfolio/#'
     },
     {
         key: 3,
         name: 'Services',
-        //path: 'localhost:3000/portfolio',
-        path : 'https://zidanMehedi.github.io/portfolio'
+        //path: 'localhost:3000/portfolio/#',
+        path : 'https://zidanMehedi.github.io/portfolio/#'
     },
     {
         key: 4,
         name: 'Skills',
-        //path: 'localhost:3000/portfolio',
-        path : 'https://zidanMehedi.github.io/portfolio'
+        //path: 'localhost:3000/portfolio/#',
+        path : 'https://zidanMehedi.github.io/portfolio/#'
     },
     {
         key: 5,
         name: 'Contact',
-        //path: 'localhost:3000/portfolio',
-        path : 'https://zidanMehedi.github.io/portfolio'
+        //path: 'localhost:3000/portfolio/#',
+        path : 'https://zidanMehedi.github.io/portfolio/#'
     },
 ];
 
